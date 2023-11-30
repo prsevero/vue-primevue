125ead99332753c26c07134b220ed6f777fe2587
Paulo Ricardo Severo <prs.severo@gmail.com>
2023-11-30 18:25:34 -0300

    Datatable: adds filter, sort and session.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import ColumnGroup from 'primevue/columngroup' // optional
 import Row from 'primevue/row' // optional
+import { FilterMatchMode } from 'primevue/api'
 import { ProductService } from '@/service/ProductService'
 
 const columns = [
-  { field: 'code', header: 'Code' },
-  { field: 'name', header: 'Name' },
+  { field: 'code', header: 'Code', sortable: true },
+  { field: 'name', header: 'Name', sortable: true },
   { field: 'category', header: 'Category' },
-  { field: 'quantity', header: 'Quantity' }
+  { field: 'quantity', header: 'Quantity', sortable: true }
 ]
 
 const handleRefresh = () => {
@@ -23,14 +25,18 @@ onMounted(() => {
 })
 
 const products = ref()
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 </script>
 
 <template>
   <div class="card">
-    <DataTable :value="products" tableStyle="min-width: 50rem">
+    <DataTable v-model:filters="filters" :globalFilterFields="['code', 'name', 'category', 'quantity']" resizableColumns scrollable sortMode="multiple" removableSort :value="products" paginator :rows="10" :rowsPerPageOptions="[10, 20, 50]" stripedRows stateStorage="local" stateKey="datatable-state">
       <template #header>
         <div class="datatable__header">
           <span>Products</span>
+          <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
           <Button @click="handleRefresh">Refresh</Button>
         </div>
       </template>
@@ -39,6 +45,7 @@ const products = ref()
         :key="col.field"
         :field="col.field"
         :header="col.header"
+        :sortable="col.sortable"
       ></Column>
     </DataTable>
   </div>
